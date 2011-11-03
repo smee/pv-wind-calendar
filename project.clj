bf21677e4bb007a08af0d06020798af1d3a0f292
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2011-11-03 21:51:32 +0100

    ical test

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject noir-mongo-heroku "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.0"]]
+                           [noir "1.2.0"]
+                           [org.mnode.ical4j/ical4j "1.0.2"]]
             :main noir-mongo-heroku.server)
 
--- src/noir_mongo_heroku/views/welcome.clj
+++ src/noir_mongo_heroku/views/welcome.clj
@@ -3,8 +3,30 @@
             [noir.content.pages :as pages])
   (:use noir.core
         hiccup.core
-        hiccup.page-helpers))
+        hiccup.page-helpers)
+  (:import [net.fortuna.ical4j.model Calendar Property Date]
+           [net.fortuna.ical4j.model.property ProdId Version CalScale]
+           net.fortuna.ical4j.model.component.VEvent
+           net.fortuna.ical4j.model.parameter.Value
+           net.fortuna.ical4j.util.UidGenerator))
 
 (defpage "/welcome" []
          (common/layout
            [:p "Welcome to noir-mongo-heroku"]))
+
+(defpage "/cal" []
+  (let [cal (Calendar.)
+        _ (doto (.getProperties cal)
+            (.add (ProdId. "-//Steffen Dienst//test 1.0//EN"))
+            (.add Version/VERSION_2_0)
+            (.add CalScale/GREGORIAN))
+        now (System/currentTimeMillis)
+        event (doto (VEvent. (Date. now) "kleiner Test genau heute")
+                ;(.. getProperties (getProperty Property/DTSTART) getParameters (add Value/DATE))
+                (.. getProperties (add (.generateUid (UidGenerator. "1")))))
+        output (net.fortuna.ical4j.data.CalendarOutputter.)
+        buf (java.io.StringWriter.)]
+    (.. cal getComponents (add event))
+    (.output output cal buf)
+    (str buf)
+    ))
