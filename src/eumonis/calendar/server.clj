434b897608923ccf7de9f6a3cb83d7d4d6b52387
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2011-11-28 14:31:00 +0100

    [fix] use internal context root 'cal'

--- src/eumonis/calendar/server.clj
+++ src/eumonis/calendar/server.clj
@@ -12,12 +12,12 @@
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "8080"))]
+        port (Integer. (get (System/getenv) "PORT" "8081"))]
     (server/start port {:mode mode
                         :ns 'eumonis.calendar
                         })))
 
 (def handler (server/gen-handler {:mode :dev
                                   :ns 'eumonis.calendar
-                                  :base-url "/eumonis-calendar"}))
+                                  :base-url "/cal"}))
 
